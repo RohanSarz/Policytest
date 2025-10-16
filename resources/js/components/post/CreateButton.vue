b8e699f34a807c3a38abb3559c71348315516aec
Rohan <rjrpic@gmail.com>
2025-10-17 00:19:13 +0600

    man git is tough

--- resources/js/components/post/CreateButton.vue
+++ resources/js/components/post/CreateButton.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-<<<<<<< HEAD
 import Button from "@/components/ui/button/Button.vue";
 import { Link } from "@inertiajs/vue3";
 </script>
@@ -8,13 +7,4 @@ import { Link } from "@inertiajs/vue3";
     <Link :href="route('posts.create')">
         <Button variant="outline">Create Post</Button>
     </Link>
-=======
-import Button from "../ui/button/Button.vue";
-</script>
-
-<template>
-    <Link :as="Button" variant="outline" :href="route('posts.create')"
-        >Create</Link
-    >
->>>>>>> 2a79c7e3c0681e4b9c4e00c23d755f56846b1f8b
 </template>
